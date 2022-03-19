3b706592446f9d72ce01ecfd3ad582b6b967db62
Milena Lazzaretti <mlazzare@student.s19.be>
2022-03-19 11:34:50 +0100

    observers + value comp

--- src/containers/map.hpp
+++ src/containers/map.hpp
@@ -6,7 +6,7 @@
 /*   By: mlazzare <mlazzare@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 07:34:38 by mlazzare          #+#    #+#             */
-/*   Updated: 2022/03/18 18:44:56 by mlazzare         ###   ########.fr       */
+/*   Updated: 2022/03/19 11:34:07 by mlazzare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,9 @@ namespace ft
 	class map
 	{
 		// VAR TYPES
+
 		public:
-			class 												Iterator;
-			class 												value_compare;
+			class 												value_compare; // defined at the very end
 
 			typedef	T											mapped_type;
             typedef Key                                         key_type;
@@ -46,15 +46,19 @@ namespace ft
             typedef Compare                                     key_compare;
 			typedef Alloc										allocator_type;
 
+		private:
+			typedef RBTree<value_type, key_compare, allocator_type>		rbtree;
+
+		public:
 			typedef value_type&			                        reference;
 			typedef const  	value_type&                     	const_reference;
 			typedef typename Alloc::pointer			        	pointer;
 			typedef typename Alloc::const_pointer		    	const_pointer;
 
-			typedef Iterator									iterator;
-			typedef const iterator								const_iterator;
-			typedef ft::reverse_iterator< iterator >			reverse_iterator;
-			typedef const ft::reverse_iterator< iterator >		const_reverse_iterator;
+			typedef ft::rbtree::Iterator							iterator;
+			typedef const ft::rbtree::Iterator						const_iterator;
+			typedef ft::reverse_iterator< iterator >				reverse_iterator;
+			typedef const ft::reverse_iterator< const_iterator >	const_reverse_iterator;
 			
 
 		private:
@@ -87,19 +91,35 @@ namespace ft
 				return *this;
 			}
 
-		// capacity
-		bool			empty( void ) const				{	return	_tree.empty();	};
-		size_type 		size( void ) const				{	return	_tree.height();	};
-		size_type 		max_size() const				{	return	_tree.max_size();	};
+			// capacity
+			bool			empty( void ) const				{	return	_tree.empty();	};
+			size_type 		size( void ) const				{	return	_tree.height();	};
+			size_type 		max_size() const				{	return	_tree.max_size();	};
 
-		// element access
-		mapped_type& operator[] ( const key_type& k )	{	return (*((this->insert(make_pair(k,mapped_type()))).first)).second;					};
-		mapped_type& at (const key_type& k)				{	try {	} catch (std::out_of_range const& e) {	std::cout << e.what() << '\n';    }};
-		const mapped_type& at (const key_type& k) const	{	try {	} catch (std::out_of_range const& e) {	std::cout << e.what() << '\n';    }};
+			// element access
+			mapped_type& operator[] ( const key_type& k )	{	return (*((this->insert(make_pair(k,mapped_type()))).first)).second;					};
+			mapped_type& at (const key_type& k)				{	try {	} catch (std::out_of_range const& e) {	std::cout << e.what() << '\n';    }};
+			const mapped_type& at (const key_type& k) const	{	try {	} catch (std::out_of_range const& e) {	std::cout << e.what() << '\n';    }};
 
+			// observers
+			key_compare 	key_comp() const				{	return _compare;				};
+			value_compare 	value_comp() const;				{	return value_compare(_compare);	};
 
-		// allocator
-		allocator_type	get_allocator( void ) const	{	return this->_alloc;	};
+			// allocator
+			allocator_type	get_allocator( void ) const	{	return this->_alloc;	};
+
+			// public classes
+			class value_compare
+			{   
+				protected:
+					key_compare		comp;
+					value_compare (key_compare c) : comp(c) {}  // constructed with map's comparison object
+				public:
+					typedef bool 			result_type;
+					typedef value_type 		first_argument_type;
+					typedef value_type 		second_argument_type;
+					bool operator() ( const value_type& x, const value_type& y ) const { 		return comp(x.first, y.first);			};
+			}
 
 	};
 

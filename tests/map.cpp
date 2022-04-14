cf0700cfed6399ee0523525f7ed2c23daaadd083
Milena Lazzaretti <mlazzare@student.s19.be>
2022-04-14 03:32:18 +0200

    add tests complete

--- tests/map.cpp
+++ tests/map.cpp
@@ -6,7 +6,7 @@
 /*   By: mlazzare <mlazzare@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 08:57:16 by mlazzare          #+#    #+#             */
-/*   Updated: 2022/04/14 03:24:49 by mlazzare         ###   ########.fr       */
+/*   Updated: 2022/04/14 03:32:05 by mlazzare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -492,6 +492,164 @@ void	ft_valuecomp(void)
 	} while ( mymap.value_comp()(*it++, highest) );
 }
 
+void	std_find(void)
+{
+	std::map<char,int> mymap;
+	std::map<char,int>::iterator it;
+
+	mymap['a']=50;
+	mymap['b']=100;
+	mymap['c']=150;
+	mymap['d']=200;
+
+	it = mymap.find('b');
+	if (it != mymap.end())
+		mymap.erase (it);
+
+	// print content:
+	std::cout << "elements in mymap:" << '\n';
+	std::cout << "a => " << mymap.find('a')->second << '\n';
+	std::cout << "c => " << mymap.find('c')->second << '\n';
+	std::cout << "d => " << mymap.find('d')->second << '\n';
+}
+
+void	ft_find(void)
+{
+	ft::map<char,int> mymap;
+	ft::map<char,int>::iterator it;
+
+	mymap['a']=50;
+	mymap['b']=100;
+	mymap['c']=150;
+	mymap['d']=200;
+
+	it = mymap.find('b');
+	if (it != mymap.end())
+		mymap.erase (it);
+
+	// print content:
+	std::cout << "elements in mymap:" << '\n';
+	std::cout << "a => " << mymap.find('a')->second << '\n';
+	std::cout << "c => " << mymap.find('c')->second << '\n';
+	std::cout << "d => " << mymap.find('d')->second << '\n';
+}
+
+void	std_count(void)
+{
+	std::map<char,int> mymap;
+	char c;
+
+	mymap ['a']=101;
+	mymap ['c']=202;
+	mymap ['f']=303;
+
+	for (c='a'; c<'h'; c++)
+	{
+		std::cout << c;
+		if (mymap.count(c)>0)
+		std::cout << " is an element of mymap.\n";
+		else 
+		std::cout << " is not an element of mymap.\n";
+	}
+}
+
+void	ft_count(void)
+{
+	ft::map<char,int> mymap;
+	char c;
+
+	mymap ['a']=101;
+	mymap ['c']=202;
+	mymap ['f']=303;
+
+	for (c='a'; c<'h'; c++)
+	{
+		std::cout << c;
+		if (mymap.count(c)>0)
+		std::cout << " is an element of mymap.\n";
+		else 
+		std::cout << " is not an element of mymap.\n";
+	}
+}
+
+void	std_bounds(void)
+{
+	std::map<char,int> mymap;
+	std::map<char,int>::iterator itlow,itup;
+
+	mymap['a']=20;
+	mymap['b']=40;
+	mymap['c']=60;
+	mymap['d']=80;
+	mymap['e']=100;
+
+	itlow=mymap.lower_bound ('b');  // itlow points to b
+	itup=mymap.upper_bound ('d');   // itup points to e (not d!)
+
+	mymap.erase(itlow,itup);        // erases [itlow,itup)
+
+	// print content:
+	for (std::map<char,int>::iterator it=mymap.begin(); it!=mymap.end(); ++it)
+		std::cout << it->first << " => " << it->second << '\n';
+}
+
+void	ft_bounds(void)
+{
+	ft::map<char,int> mymap;
+	ft::map<char,int>::iterator itlow,itup;
+
+	mymap['a']=20;
+	mymap['b']=40;
+	mymap['c']=60;
+	mymap['d']=80;
+	mymap['e']=100;
+
+	itlow=mymap.lower_bound ('b');  // itlow points to b
+	itup=mymap.upper_bound ('d');   // itup points to e (not d!)
+
+	mymap.erase(itlow,itup);        // erases [itlow,itup)
+
+	// print content:
+	for (ft::map<char,int>::iterator it=mymap.begin(); it!=mymap.end(); ++it)
+		std::cout << it->first << " => " << it->second << '\n';
+}
+
+void	std_equalrange(void)
+{
+	std::map<char,int> mymap;
+
+	mymap['a']=10;
+	mymap['b']=20;
+	mymap['c']=30;
+
+	std::pair<std::map<char,int>::iterator,std::map<char,int>::iterator> ret;
+	ret = mymap.equal_range('b');
+
+	std::cout << "lower bound points to: ";
+	std::cout << ret.first->first << " => " << ret.first->second << '\n';
+
+	std::cout << "upper bound points to: ";
+	std::cout << ret.second->first << " => " << ret.second->second << '\n';
+}
+
+void	ft_equalrange(void)
+{
+	ft::map<char,int> mymap;
+
+	mymap['a']=10;
+	mymap['b']=20;
+	mymap['c']=30;
+
+	ft::pair<std::map<char,int>::iterator,std::map<char,int>::iterator> ret;
+	ret = mymap.equal_range('b');
+
+	std::cout << "lower bound points to: ";
+	std::cout << ret.first->first << " => " << ret.first->second << '\n';
+
+	std::cout << "upper bound points to: ";
+	std::cout << ret.second->first << " => " << ret.second->second << '\n';
+}
+
 void	std_allocator(void)
 {
 	int psize;
@@ -560,8 +718,7 @@ void	test_start(void)
 	caller(ft_valuecomp, std_valuecomp, "value_comp"); // observers
 	caller(ft_find, std_find, "find"); // operations
 	caller(ft_count, std_count, "count"); // operations
-	caller(ft_lowerbound, std_lowerbound, "lower_bound"); // operations
-	caller(ft_equalrange, std_upperbound, "upper_bound"); // operations
+	caller(ft_bounds, std_bounds, "lower and upper bound"); // operations
 	caller(ft_equalrange, std_equalrange, "equal_range"); // operations
 	caller(ft_allocator, std_allocator, "allocator"); // allocator
 }
